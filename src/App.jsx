2bbd81fe591650afa6194dbce56c2ac1924bae79
RimjhimD <rimjhimdey24@gmail.com>
2026-05-01 19:28:00 +0600

    add files to git

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+
+    <h1 className ="text-blue-600  dark:text-sky-400 border-2 p-4  rounded-xl " >
+      Hello React
+    </h1>
+
+    <div className = "max-w-sm bg-white border5 border-gray-200 rounded-xl mt-8 shadow overflow-hidden transition-shadow">
+      <img src ="https://i.ytimg.com/vi/Es7GfE1qOC0/maxresdefault.jpg"
+      alt="demo image"
+      />
+    </div>
+    <div>
+      <h2>Card title</h2>
+      <p>lorum ipsum lorum ipsum sbjhdbsejh djhwbgdjhqvbwc cdwdqwdqwdqwd</p>
+      <button>Buy now</button>
+    </div>
+
+    </div>
+
+  );
+}
+
+export default App
+
